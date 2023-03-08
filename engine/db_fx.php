f7c58b1697ccb47c7c2f6faef6a1f5e7c1f429de
EGNSS4ALL <101722796+EGNSS4ALL@users.noreply.github.com>
2023-03-08 18:06:35 +0100

    Add files via upload

---
+++ engine/db_fx.php
@@ -0,0 +1,18 @@
+<?php
+
+function db_connect() {
+	$spoj = ($GLOBALS["mysqli_spoj"] = mysqli_connect("localhost",  "xxxx",  "xxxx"));
+	mysqli_select_db($GLOBALS["mysqli_spoj"], "egnss4cap");
+	mysqli_query($GLOBALS["mysqli_spoj"], "set character set UTF8"); 
+  mysqli_query($GLOBALS["mysqli_spoj"], "set names UTF8"); 
+  mysqli_query($GLOBALS["mysqli_spoj"], "charset UTF8"); 
+  mysqli_query($GLOBALS["mysqli_spoj"], "SET time_zone='+01:00'"); 
+	if (!$spoj): $error=1; echo "An error ocurred while connecting into database. Please contact an administrator."; die(''); endif;
+}
+
+function db_close() {
+	mysqli_close($GLOBALS["mysqli_spoj"]);
+}
+
+//Created for the GSA in 2020-2021. Project management: SpaceTec Partners, software development: www.foxcom.eu
+?>
